51be49dd9f0609d45382023c5e2cc84c818879e1
Israel Schroeder <israelschroederm@gmail.com>
2024-06-13 21:27:16 -0300

    API Gateway working

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Logger } from '@nestjs/common';
 
 declare const module: any;
 
+const logger = new Logger('velure-auth-service');
+
 async function bootstrap() {
-  const PORT = process.env.APP_PORT || 3001;
-  const ORIGIN_UI = process.env.ORIGIN_UI || 'http://localhost:4200';
+  const PORT = process.env.APP_PORT || 3020;
 
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: ORIGIN_UI });
-
-  await app.listen(PORT, () =>
-    console.log('Server is listening on port', PORT),
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: +PORT,
+      },
+    }
   );
+
+  await app.listen();
+  logger.log(`Authentication service is running on port ${PORT}`);
 
   if (module.hot) {
     module.hot.accept();
--- src/modules/authentication/authentication.controller.ts
+++ src/modules/authentication/authentication.controller.ts
@@ -6,15 +6,14 @@ import { CreateAuthenticationDto } from './dto/create-authentication.dto';
 
 import { User } from '@prisma/client';
 import { ILoginResponse } from './dto/login-response-dto';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('authentication')
 export class AuthenticationController {
-  constructor(private readonly authService: AuthenticationService) {}
+  constructor(private readonly authService: AuthenticationService) { }
 
-  @Post('register')
-  async register(
-    @Body() createAuthDto: CreateAuthenticationDto,
-  ): Promise<CreateAuthenticationDto> {
+  @MessagePattern({ cmd: 'register_user' })
+  async register(@Body() createAuthDto: CreateAuthenticationDto,): Promise<CreateAuthenticationDto> {
     try {
       return await this.authService.createUser(createAuthDto);
     } catch (error) {
@@ -22,18 +21,13 @@ export class AuthenticationController {
     }
   }
 
-  @Post('login')
-  async login(
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ): Promise<ILoginResponse> {
+  @MessagePattern({ cmd: 'login' })
+  async login(@Body('email') email: string, @Body('password') password: string): Promise<ILoginResponse> {
     return await this.authService.login(email, password);
   }
 
-  @Post('validate-token')
-  async validateToken(
-    @Body('token') token: string,
-  ): Promise<{ isValid: boolean }> {
+  @MessagePattern({ cmd: 'validate_token' })
+  async validateToken(@Body('token') token: string,): Promise<{ isValid: boolean }> {
     try {
       const user = await this.authService.validateAccessToken(token);
       return { isValid: !!user };
@@ -42,22 +36,22 @@ export class AuthenticationController {
     }
   }
 
-  @Get('users')
+  @MessagePattern({ cmd: 'get_users' })
   async getUsers(): Promise<User[]> {
     return await this.authService.getUsers();
   }
 
-  @Get('userById/:id')
+  @MessagePattern({ cmd: 'get_user_by_id' })
   async getUserById(@Param('id') id: string): Promise<User> {
     return await this.authService.getUserById(+id);
   }
 
-  @Get('userByEmail/:email')
+  @MessagePattern({ cmd: 'get_user_by_email' })
   async getUserByEmail(@Param('email') email: string): Promise<User> {
     return await this.authService.getUserByEmail(email);
   }
 
-  @Delete('logout')
+  @MessagePattern({ cmd: 'logout' })
   async logout(@Body() refreshToken: string): Promise<void> {
     await this.authService.logout(refreshToken);
   }
